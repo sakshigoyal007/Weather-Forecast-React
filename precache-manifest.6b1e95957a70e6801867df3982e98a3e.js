78b6a19be7fefd128f9a9b3ab19d2afe98c3abc1
sakshigoyal007 <sakshigoyal718@gmail.com>
2020-08-04 23:43:41 +0530

    Updates

---
+++ precache-manifest.6b1e95957a70e6801867df3982e98a3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78eada042450cb7c35664a8298262025",
+    "url": "/Weather-Forecast-React/index.html"
+  },
+  {
+    "revision": "bb36999859df853cbbee",
+    "url": "/Weather-Forecast-React/static/css/main.17339b61.chunk.css"
+  },
+  {
+    "revision": "46c6137663e1929b12ae",
+    "url": "/Weather-Forecast-React/static/js/2.ad5e34e5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Weather-Forecast-React/static/js/2.ad5e34e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb36999859df853cbbee",
+    "url": "/Weather-Forecast-React/static/js/main.327d072d.chunk.js"
+  },
+  {
+    "revision": "6ae30c62d06716a06fca",
+    "url": "/Weather-Forecast-React/static/js/runtime-main.4794c9da.js"
+  },
+  {
+    "revision": "0f001c005f3fddc3283a77e7c2d77c05",
+    "url": "/Weather-Forecast-React/static/media/bg1.0f001c00.jpg"
+  }
+]);
